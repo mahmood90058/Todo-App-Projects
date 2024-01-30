872adeeb356dac1421fff7bdba3d85acd687bbb2
mahmood90058 <116994957+mahmood90058@users.noreply.github.com>
2024-01-30 12:30:39 +0530

    1

--- 14-day/todo-with-routing/src/pages/About.jsx
+++ 14-day/todo-with-routing/src/pages/About.jsx
@@ -1,9 +1,19 @@
 import styles from "./About.module.css";
 function About() {
   return (
+    <>
     <div className={styles.about}>
       <h1>About Page</h1>
     </div>
+
+    <div className={styles.ptodo}>
+      <p>This is the Todo App where you can complete your daily task..</p>
+    </div>
+
+
+    </>
+    
+    
   );
 }
 export default About;
--- 14-day/todo-with-routing/src/pages/Completed.jsx
+++ 14-day/todo-with-routing/src/pages/Completed.jsx
@@ -1,12 +1,14 @@
+import styles from "./Completed.module.css";
 function Completed({ todos }) {
   const completedTodos = todos.filter((todo) => todo.completed);
   return (
     <div>
       {completedTodos.map((todo) => (
         <div key={todo.id}>
-          <h2>{todo.title}</h2>
+          <h2 className={styles.todo} >{todo.title}</h2>
         </div>
       ))}
+      <p className={styles.para}>Task Completed</p>
     </div>
   );
 }
